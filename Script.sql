f83fe7e37a2e6ce3f40bbc73baa2986e985d599f
Ramon Gall <oliveiraagall@gmail.com>
2023-08-19 16:39:51 -0300

    SubindoArquivoSql

---
+++ Script.sql
@@ -0,0 +1,22 @@
+PRAGMA foreign_keys = ON;
+
+CREATE TABLE usuario(
+	codigo integer primary key autoincrement not null,
+	nome text not null,
+	nomedeusuario text not null,
+	cpf text not null,
+	datadenascimento text not null,
+	pais text not null,
+	estado text not null,
+	cidade text not null,
+	rua text not null,
+	numero integer,
+	complemento text,
+	Telefone_pessoal integer not null,
+	Telefone_profissional integer
+);
+CREATE TABLE categoria(
+codigo integer primary key autoincrement not null,
+nome text,
+descricao text
+);

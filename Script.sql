aa5f162a9c80d2d6036b61e1977d4575a5a1b3cd
thomas victor <thomasvictor2010@gmail.com>
2023-08-21 22:23:49 -0300

    adcionando o insert com os nomes cadastrados

--- Script.sql
+++ Script.sql
@@ -5,7 +5,7 @@ CREATE TABLE usuario(
 	nome text not null,
 	nomedeusuario text not null,
 	cpf text not null,
-	datadenascimento text not null,
+	datadenascimento date not null,
 	pais text not null,
 	estado text not null,
 	cidade text not null,
@@ -20,7 +20,6 @@ CREATE TABLE categoria(
 	nome text,
 	descricao text
 );
-
 CREATE TABLE pedido (
 	codigo integer primary key autoincrement not null,
 	codigo_usuario integer,
@@ -48,3 +47,97 @@ CREATE TABLE produto_pedido (
 	foreign key (codigo_produto) references produto(codigo),
 	foreign key (codigo_pedido) references pedido(codigo)
 );
+
+INSERT into usuario (
+	nome,
+	nomedeusuario,
+	cpf,
+	datadenascimento,
+	pais,
+	estado,
+	cidade,
+	rua,
+	numero,
+	Telefone_pessoal,
+	Telefone_profissional
+) VALUES (
+	"Thomas Victor",
+	"thomasten22",
+	33355577789,
+	"1999-06-18",
+	"Brasil",
+	"RJ",
+	"Petrópolis",
+	"Rua joaquim José",
+	161,
+	24988552230,
+	2422231590
+),(
+	"Lucas Oliveira",
+	"LucasOliveira",
+	11122266600,
+	"1990-01-02",
+	"Brasil",
+	"RJ",
+	"Petrópolis",
+	"Rua Henrique José",
+	16,
+	24988552233,
+	2422211590
+),(
+	"Thadeu Goes",
+	"Thadeugoes",
+	33244377680,
+	"1993-04-11",
+	"Brasil",
+	"RJ",
+	"Petrópolis",
+	"Rua José Henrique",
+	6,
+	24988552240,
+	2422221590
+),(
+	"Pedro Teixeira",
+	"Pedroteixeira",
+	55590986832,
+	"2000-02-20",
+	"Brasil",
+	"RJ",
+	"Petrópolis",
+	"Rua José Emanuel",
+	65,
+	24988553340,
+	2422234570
+),(
+	"Ramon Gall",
+	"Ramongall",
+	65679738209,
+	"1991-09-09",
+	"Brasil",
+	"RJ",
+	"Petrópolis",
+	"Rua José Emanuel",
+	65,
+	24988326789,
+	2422315978
+),(
+	"Matheus Botelho",
+	"Matheusbotelho",
+	18345975709,
+	"2004-08-24",
+	"Brasil",
+	"RJ",
+	"Petrópolis",
+	"Rua Sao Tomas De Aquino",
+	5,
+	249993756049,
+	2422238970
+);
+
+SELECT * FROM usuario u 
+
+
+
+	
+
+
